b49d82b8ce74d39455aa8fd5554ae9d973352f7c
Konstantin Alekseevich <ept.13@inbox.ru>
2024-03-17 11:52:16 +0300

    dasasasas

--- tryer228.py
+++ tryer228.py
@@ -2,3 +2,5 @@ def foo(aim):
     print(f"Hi,here is new file in a remote repositorium,created for{aim}.GLhf")
 
 print('Внес изменения в функциюю foo там print не работал, синтаксис нарушен был')
+print('ИЗмени ссылку с HTTPS на SSH ')
+print('ИЗмени ссылку с HTTPS на SSH  еще раз')
